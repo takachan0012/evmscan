fddb3d0da52763504e4e4c42970bf2ef6a292bf3
takachan0012 <jj4068851@gmail.com>
2024-03-02 04:59:19 +0700

    feat: /bnb command

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-// import "dotenv/config";
 require('dotenv').config()
-const key = process.env.KEY;
-console.log(key);
-async function test() {
+const TelegramBot = require("node-telegram-bot-api");
+const BSC_SCAN_TOKEN = process.env.KEY;
+const BOT_TOKEN = process.env.BOT;
+const patternGetAddress = /^\/\w+\s+(\w+)/; // get Second word of sentece "/bnb 0xfffxxxx"
+const bnb = /^\/bnb\b/;
+const evmBot = new TelegramBot(BOT_TOKEN, { polling: true });
+evmBot.onText(bnb, async msg => {
+    let response;
+    const id = msg.from.id;
+    const message = msg.text.match(patternGetAddress);
+    if (message && message.length > 1) {
+        const address = message[1];
+        response = await test(address);
+    }
+    evmBot.sendMessage(id, "Your Balance: " + response)
+})
+
+async function test(address) {
     try {
-        const result = await fetch("https://api.bscscan.com/api?module=account&action=balance&address=0xee226379dB83CfFC681495730c11fDDE79BA4c0C&apikey=" + key)
+        const result = await fetch("https://api.bscscan.com/api?module=account&action=balance&address=" + address + "&apikey=" + BSC_SCAN_TOKEN)
         if (!result.ok) {
             throw new Error("getting Error")
         };
         const data = await result.json();
         const getWei = data.result;
         data.result = weiToBNB(getWei);
-        console.log(data);
+        return data.result + " BNB";
     } catch (e) {
-        console.log({ e });
+        return e.message;
     }
 }
-
-test();
-
-
 function weiToBNB(wei) {
     return wei / 1000000000000000000; // 1 BNB = 1 
 }
